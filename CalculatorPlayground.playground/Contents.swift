68fc8d5a7c5819d7b9655f30595170e20ac325a5
ghnn-n <cgh6470@gmail.com>
2025-03-10 16:20:20 +0900

    Lv.1

---
+++ CalculatorPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+class Calculator {
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+    
+    func multiply(_ a: Int, _ b: Int) -> Int {
+        return a * b
+    }
+    
+    func divide(_ a: Int, _ b: Int) -> Int {
+        return a / b
+    }
+}
+
+var question = Calculator()
+
+print(question.add(20, 49))
+print(question.subtract(230, 410))
+print(question.multiply(23, 132))
+print(question.divide(125, 5))
