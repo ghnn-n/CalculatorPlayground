08b45d326d0b55c388b06dec1d944ddf5450f9ce
ghnn-n <cgh6470@gmail.com>
2025-03-12 18:11:09 +0900

    Comment: 주석 생성

--- CalculatorPlayground.playground/Contents.swift
+++ CalculatorPlayground.playground/Contents.swift
@@ -1,7 +1,9 @@
+// 추상화된 프로토콜 생성
 protocol AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int
 }
 
+// 각 클래스들에 프로토콜 채택
 class AddOperation: AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber + secondNumber
@@ -33,30 +35,35 @@ class RemainderOperation: AbstractOperation {
 }
 
 class Calculator {
-    private var operate: AbstractOperation
+    private var operate: AbstractOperation  // 프로퍼티에 프로토콜 채택
     
-    init(operate: AbstractOperation) {
+    init(operate: AbstractOperation) {  // 초기화
         self.operate = operate
     }
     
+    // 프로퍼티의 operator를 변경하는 메서드
     func changeOperate(_ newOperate: AbstractOperation) {
         self.operate = newOperate
     }
     
+    // 각각 operator의 해당하는 계산을 해줄 메서드
     func calculating(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         operate.result(firstNumber, secondNumber)
     }
 }
 
+// 클래스들의 인스턴스 생성
 let add = AddOperation()
 let subtract = SubtractOperation()
 let multiply = MultiplyOperation()
 let divide = DivideOperation()
 let remainder = RemainderOperation()
 
+// 메인 클래스의 인스턴스 생성 및 구동
 var question = Calculator(operate: add)
 print(question.calculating(10, 20))
 
+// 다른 식을 사용하려면 changeOperate 메서드를 사용해야 함
 question.changeOperate(subtract)
 print(question.calculating(10, 20))
 
