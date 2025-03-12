eeffd3de718736e87d5cfea12d1a5f3da15e6111
ghnn-n <cgh6470@gmail.com>
2025-03-12 15:18:25 +0900

    Style: 의존성역전원칙 준수, Protocol 추가

--- CalculatorPlayground.playground/Contents.swift
+++ CalculatorPlayground.playground/Contents.swift
@@ -1,41 +1,66 @@
-class AddOperation {
+protocol AbstractOperation {
+    func result(_ firstNumber: Int, _ secondNumber: Int) -> Int
+}
+
+class AddOperation: AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber + secondNumber
     }
 }
 
-class SubtractOperation {
+class SubtractOperation: AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber - secondNumber
     }
 }
 
-class MultiplyOperation {
+class MultiplyOperation: AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber * secondNumber
     }
 }
 
-class DivideOperation {
+class DivideOperation: AbstractOperation {
     func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber / secondNumber
     }
 }
 
-class Calculator {
-    let add = AddOperation()
-    let subtract = SubtractOperation()
-    let multiply = MultiplyOperation()
-    let divide = DivideOperation()
-    
-    func remainder(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+class RemainderOperation: AbstractOperation {
+    func result(_ firstNumber: Int, _ secondNumber: Int) -> Int {
         return firstNumber % secondNumber
     }
 }
 
-var question = Calculator()
+class Calculator {
+    var operate: AbstractOperation
+    
+    init(operate: AbstractOperation) {
+        self.operate = operate
+    }
+    
+    func calculating(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        operate.result(firstNumber, secondNumber)
+    }
+}
 
-print(question.add.result(32, 32))
-print(question.subtract.result(239, 235))
-print(question.multiply.result(23, 53))
-print(question.divide.result(234, 34))
+let add = AddOperation()
+let subtract = SubtractOperation()
+let multiply = MultiplyOperation()
+let divide = DivideOperation()
+let remainder = RemainderOperation()
+
+var question = Calculator(operate: add)
+print(question.calculating(10, 20))
+
+question.operate = subtract
+print(question.calculating(10, 20))
+
+question.operate = multiply
+print(question.calculating(10, 20))
+
+question.operate = divide
+print(question.calculating(10, 20))
+
+question.operate = remainder
+print(question.calculating(10, 20))
